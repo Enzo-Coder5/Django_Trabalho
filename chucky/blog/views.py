e59f4403cdb7278390e1001d267bfbef5819d3ab
Enzo-Coder5 <enzomelo526@gmail.com>
2025-10-09 10:54:19 -0300

    nova versão

--- chucky/blog/views.py
+++ chucky/blog/views.py
@@ -1,73 +1,47 @@
-from django.shortcuts import render
-from blog.models import Post
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.template import loader
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib import messages
+from .models import Post
 
 
-def home(request):
-    return HttpResponse("<h1>Bem-vindo ao Blog!</h1>")
-
-
+# Página inicial simples
 def home(request):
     return render(request, 'home.html')
 
 
-
-
+# Exemplo de listagem de posts
 def testing(request):
-  posts = Post.objects.all().values()
-  template = loader.get_template('home.html')
-  context = {
-    'posts': posts,
-  }
-  return HttpResponse(template.render(context, request))
+    posts = Post.objects.all().values()
+    template = loader.get_template('home.html')
+    context = {'posts': posts}
+    return HttpResponse(template.render(context, request))
 
 
-
-def login(request):
+# Login usando autenticação do Django
+def login_view(request):
     if request.method == "POST":
-        nome = request.POST.get("nome")
-        senha = request.POST.get("senha")
-        posts = Post.objects.all().values()
-        print(nome)
-        print(senha)
-        return render(request, 'home.html', locals())
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
-    return render(request, 'login.html', locals())
+        user = authenticate(request, username=username, password=password)
 
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválidos.")
 
-from .models import Pessoa
-
-
-
-def login(request):
-    if request.method == "POST":
-        nome = request.POST.get("nome")
-        senha = request.POST.get("senha")
-        lista_pessoas = []
-        pessoas = Pessoa.objects.all().values()
-        for i in pessoas:
-            p_json = {}
-            p_json['id'] = i['id']
-            p_json['usuario_id'] = i['usuario_id']
-            p_json['data_nascimento'] = i['data_nascimento']
-            usuario = User.objects.get(id=i['usuario_id'])
-            p_json['name_usuario'] = usuario.username
-            p_json['primeir_nome_usuario'] = usuario.first_name
-            p_json['email_usuario'] = usuario.email
-            lista_pessoas.append(p_json)
+    return render(request, 'login.html')
 
 
-        pessoas = lista_pessoas
-
-        return render(request, 'home.html', locals())
-
-    return render(request, 'login.html', locals())
-
-from django.contrib.auth import logout
-from django.shortcuts import render
-
+# Logout → redireciona para a tela de logout.html
 def logout_view(request):
-    logout(request)
+    auth_logout(request)
     return render(request, 'logout.html')
+
+
+# Tela com botão para recuperação de senha
+def recuperar_senha(request):
+    return render(request, 'recuperar_senha.html')
